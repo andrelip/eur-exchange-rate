a5c8cd9e0ed87710a4111cc496a10ec8b5193e56
André Souza <andrestephano@gmail.com>
2018-10-07 22:13:27 -0300

    adds mix fetch_ratings task

---
+++ lib/mix/tasks/fetch_ratings.ex
@@ -0,0 +1,10 @@
+defmodule Mix.Tasks.FetchRatings do
+  use Mix.Task
+  alias EurExchangeRate.Exchange
+
+  @shortdoc "Simply runs the EurExchangeRate.Exchange.fetch_ratings/0 function"
+  def run(_) do
+    Mix.Task.run("app.start")
+    Exchange.fetch_ratings()
+  end
+end
